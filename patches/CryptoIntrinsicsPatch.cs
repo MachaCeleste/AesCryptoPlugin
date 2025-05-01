5c1f92629f602b14e6f260826077223d212f9fae
Macha Celeste <shetah1@yahoo.com>
2025-05-01 15:36:49 -0400

    added cross mod support by making intrinsics get added to preexisting list rather than replace the list entirely fixed mod formatting

--- patches/CryptoIntrinsicsPatch.cs
+++ patches/CryptoIntrinsicsPatch.cs
@@ -3,39 +3,42 @@ using FoxCrypto;
 using Miniscript;
 
 [HarmonyPatch]
-class CryptoIntrinsicsPatch
+public class CryptoIntrinsicsPatch
 {
-    private static bool intrinsicsAdded;
-
     [HarmonyPatch(typeof(CryptoIntrinsics), "AddInstrinsics")]
-    static void Postfix()
+    class AddInstrinsicsPatch
     {
-        if (intrinsicsAdded == true)
-            return;
-        intrinsicsAdded = true;
-        Intrinsic intrinsic = Intrinsic.Create("aes");
-        intrinsic.AddParam("data");
-        intrinsic.AddParam("password");
-        intrinsic.AddParam("decrypt", 0.0);
-        intrinsic.code = delegate (TAC.Context context, Intrinsic.Result partialResult)
+        private static bool intrinsicsAdded;
+
+        static void Postfix()
         {
-            ValString valString = context.GetVar("data") as ValString;
-            if (valString == null || string.IsNullOrEmpty(valString.value))
+            if (intrinsicsAdded == true)
+                return;
+            intrinsicsAdded = true;
+            Intrinsic intrinsic = Intrinsic.Create("aes");
+            intrinsic.AddParam("data");
+            intrinsic.AddParam("password");
+            intrinsic.AddParam("decrypt", 0.0);
+            intrinsic.code = delegate (TAC.Context context, Intrinsic.Result partialResult)
             {
-                return new Intrinsic.Result("aes: Invalid data");
-            }
-            ValString valString1 = context.GetVar("password") as ValString;
-            if (valString1 == null || string.IsNullOrEmpty(valString1.value) || valString1.value.Length < 3 || valString1.value.Length > 32)
-            {
-                return new Intrinsic.Result("aes: Password must be 3-32 characters");
-            }
-            ValNumber valNumber = context.GetVar("decrypt") as ValNumber;
-            var output = CryptoInterface.Run(valString.value, valString1.value, valNumber.BoolValue() ? "dec" : "enc");
-            if (output == null)
-            {
-                return new Intrinsic.Result("aes: Incorrect password or malformed data");
-            }
-            return new Intrinsic.Result(output);
-        };
+                ValString valString = context.GetVar("data") as ValString;
+                if (valString == null || string.IsNullOrEmpty(valString.value))
+                {
+                    return new Intrinsic.Result("aes: Invalid data");
+                }
+                ValString valString1 = context.GetVar("password") as ValString;
+                if (valString1 == null || string.IsNullOrEmpty(valString1.value) || valString1.value.Length < 3 || valString1.value.Length > 32)
+                {
+                    return new Intrinsic.Result("aes: Password must be 3-32 characters");
+                }
+                ValNumber valNumber = context.GetVar("decrypt") as ValNumber;
+                var output = CryptoInterface.Run(valString.value, valString1.value, valNumber.BoolValue() ? "dec" : "enc");
+                if (output == null)
+                {
+                    return new Intrinsic.Result("aes: Incorrect password or malformed data");
+                }
+                return new Intrinsic.Result(output);
+            };
+        }
     }
 }
---
+++ patches/GreyInterpreterPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using Miniscript;
+using System.Reflection;
+
+[HarmonyPatch]
+public class GreyInterpreterPatch
+{
+    [HarmonyPatch(typeof(GreyInterpreter), "CryptoLibType")]
+    class CryptoLibTypePatch
+    {
+        static void Postfix(GreyInterpreter __instance, ref GreyMap __result)
+        {
+            FieldInfo fieldInfo = AccessTools.Field(typeof(GreyInterpreter), "_cryptoLibType");
+            GreyMap _cryptoLibType = fieldInfo.GetValue(__instance) as GreyMap;
+            if (!_cryptoLibType.ContainsKey("aes"))
+            {
+                _cryptoLibType["aes"] = Intrinsic.GetByName("aes").GetFunc();
+            }
+            fieldInfo.SetValue(__instance, _cryptoLibType);
+            __result = _cryptoLibType;
+        }
+    }
+}
